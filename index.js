4d9a0dee924cb93e5e75aae645a4bcd9331873e2
Kevx17 <kevinkekoller1@gmail.com>
2024-08-20 16:17:26 +0200

    changed some design and worked on dashboard sample

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ new_game = document.getElementById("host")
 join_game = document.getElementById("join")
 
 new_game.addEventListener("click", function() {
-    window.location.href = "http://127.0.0.1:5500/new_game.html";
+    window.location.href = "http://127.0.0.1:5500/NFL_quizzGame/new_game.html";
 });
 
 join_game.addEventListener("click", function() {
-    window.location.replace("http://127.0.0.1:5500/join_game.html");
+    window.location.replace("http://127.0.0.1:5500/NFL_quizzGame/join_game.html");
 });
